35fc46f870daa2c6fa1ab0bb7c13cf1df56d336b
Yash Dahiya <yash.dahiya@myyhashstash.com>
2024-03-20 19:21:38 +0530

    Added Animator to Player, Created Animator Manager Script
    This commit introduces significant changes related to player animation management. The following modifications have been made:
    
    - Added Animator Component to Player: Integrated an Animator component into the player GameObject to control character animations.
    - Created AnimatorManager Script: Developed a new script named "AnimatorManager.cs" responsible for managing player animations and transitions.

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     PlayerControls playerControls;
+    AnimatorManager animatorManager;
 
     public Vector2 movementInput;
+    public float moveAmount;
     public float verticalInput;
     public float horizontalInput;
 
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
 
     private void OnEnable()
     {
@@ -38,6 +44,8 @@ public class InputManager : MonoBehaviour
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
 
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
     }
 
 
